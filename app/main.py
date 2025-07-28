74d1b39daccb7896d8a2c7c273f6e1ac55aa46ef
ReOneSec <reonesecapis@gmail.com>
2025-07-29 04:12:58 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,67 +1,42 @@
-import socket
-import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
-import sys
+import argparse
 
-# Parse --directory argument
-directory = "."
-if "--directory" in sys.argv:
-    idx = sys.argv.index("--directory")
-    if idx + 1 < len(sys.argv):
-        directory = sys.argv[idx + 1]
+class CustomHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path.startswith("/files/"):
+            filename = self.path[len("/files/"):]
+            content_length = int(self.headers.get("Content-Length", 0))
+            data = self.rfile.read(content_length)
 
-HOST = 'localhost'
-PORT = 4221
+            try:
+                filepath = os.path.join(self.server.directory, filename)
+                with open(filepath, "wb") as f:
+                    f.write(data)
 
-def handle_client(conn):
-    try:
-        request = conn.recv(1024).decode()
-        lines = request.splitlines()
-        if not lines:
-            return
-
-        method, path, _ = lines[0].split()
-
-        # Only GET method supported
-        if method != "GET":
-            conn.sendall(b"HTTP/1.1 405 Method Not Allowed\r\n\r\n")
-            return
-
-        if path.startswith("/files/"):
-            filename = path[len("/files/"):]
-            filepath = os.path.join(directory, filename)
-
-            if os.path.isfile(filepath):
-                with open(filepath, "rb") as f:
-                    content = f.read()
-                headers = [
-                    "HTTP/1.1 200 OK",
-                    "Content-Type: application/octet-stream",
-                    f"Content-Length: {len(content)}",
-                    "",
-                    ""
-                ]
-                response = "\r\n".join(headers).encode() + content
-                conn.sendall(response)
-            else:
-                conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-
+                self.send_response(201)
+                self.end_headers()
+            except Exception as e:
+                print("Error:", e)
+                self.send_response(500)
+                self.end_headers()
         else:
-            # Default root handler
-            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            self.send_response(404)
+            self.end_headers()
 
-    finally:
-        conn.close()
+    def log_message(self, format, *args):
+        return  # Disable default logging
 
-def start_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-
-        while True:
-            conn, _ = s.accept()
-            thread = threading.Thread(target=handle_client, args=(conn,))
-            thread.start()
+def run(directory, port=4221):
+    handler = CustomHandler
+    server = HTTPServer(("", port), handler)
+    server.directory = directory
+    print(f"Server running at http://localhost:{port}/")
+    server.serve_forever()
 
 if __name__ == "__main__":
-    start_server()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory", required=True)
+    args = parser.parse_args()
+
+    run(directory=args.directory)
